ec1c1c11a6769678bf6be5258bff20da51c25d95
Brian Huang <zbrianhuang1@gmail.com>
2022-11-15 18:54:54 -0500

    iujhkj

---
+++ LList.java
@@ -0,0 +1,11 @@
+public class LList {
+    private Node head;
+    private Node tail;
+    private int counter;
+    public LList(){
+        head = null;
+        tail = null;
+        counter = 0;
+    }
+    
+}
